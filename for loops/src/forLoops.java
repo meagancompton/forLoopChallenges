657bd4f738119dbf8ea3271aef07115395ca4898
meagan.compton <meagan.compton@mu-c1-07.mullen.pvt.k12.co.us>
2018-08-27 09:10:36 -0600

    done with 1-3 working on 4

--- for loops/src/forLoops.java
+++ for loops/src/forLoops.java
@@ -5,10 +5,11 @@ public class forLoops
 		public static void main(String[] args)
 			{
 				challengeOne();
-//				challengeTwo();
-//				challengeThree();
-//				ChallengeFour();
+				challengeTwo();
+				challengeThree();
+				ChallengeFour();
 			}
+		
 		public static void challengeOne()
 		{
 			for (int i = 1; i < 6; i++)
@@ -16,4 +17,28 @@ public class forLoops
 					System.out.println(i);
 				}
 		}
+		
+		public static void challengeTwo()
+		{
+			for (int i = 2; i < 10; i = i + 2) 
+				{
+					System.out.println(i + " I love Mullen");
+				}		
+		}
+		
+		public static void challengeThree()
+		{
+			for (int i = 1; i < 6; i++)
+				{
+					System.out.print(i);
+				}
+		}
+		
+		public static void challengeFour()
+		{
+			for (int i = 10; i > 10; i--)
+				System.out.println(i);
+		}
+		
+		
 	}
