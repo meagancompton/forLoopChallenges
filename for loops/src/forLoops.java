c50db5adb208968f2a47664ca904e3b809fa075a
meagan.compton <meagan.compton@mu-c1-07.mullen.pvt.k12.co.us>
2018-08-27 08:46:36 -0600

    begining of for loop challenges

---
+++ for loops/src/forLoops.java
@@ -0,0 +1,16 @@
+
+public class forLoops
+	{
+
+		public static void main(String[] args)
+			{
+				challengeOne();
+				challengeTwo();
+				challengeThree();
+				ChallengeFour();
+			}
+		public static void challengeOne()
+		{
+			
+		}
+	}
