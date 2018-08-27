fe5198d1e108a98ff2428f926b63c4ed4f29049b
meagan.compton <meagan.compton@mu-c1-07.mullen.pvt.k12.co.us>
2018-08-27 08:56:36 -0600

    challenge 1 finished

--- for loops/src/forLoops.java
+++ for loops/src/forLoops.java
@@ -5,12 +5,15 @@ public class forLoops
 		public static void main(String[] args)
 			{
 				challengeOne();
-				challengeTwo();
-				challengeThree();
-				ChallengeFour();
+//				challengeTwo();
+//				challengeThree();
+//				ChallengeFour();
 			}
 		public static void challengeOne()
 		{
-			//hgouihuio;hjiokl
+			for (int i = 1; i < 6; i++)
+				{
+					System.out.println(i);
+				}
 		}
 	}
